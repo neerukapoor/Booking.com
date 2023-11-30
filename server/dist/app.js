ebe43dc218e8a3965dc769fd16992931a5990808
Neeru Kapoor <neerukapoor@microsoft.com>
2023-11-30 21:30:21 +0530

    Small change to Add admin side authentication

--- server/dist/app.js
+++ server/dist/app.js
@@ -7,8 +7,10 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 const index_1 = __importDefault(require("./routes/index"));
+const adminAuth_1 = __importDefault(require("./routes/adminAuth"));
 app.use(express_1.default.json());
 app.use("/", index_1.default);
+app.use("/admin", adminAuth_1.default);
 app.listen(3000, () => {
     console.log(`Server listning on port ${PORT}`);
 });
