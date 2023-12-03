6562059e5fe1f7345e18bf1d1b5a51bbac600295
Pratyush Gupta <56015449+pratyushgguptaa@users.noreply.github.com>
2023-12-03 11:49:47 +0530

    #14 Merged PR: Password Hashing
    * [server] add eslint and nodemon
    
    * add prettier rc config
    
    * remove lint for json config
    
    * refactor authController and store password hash
    
    * fix signup for unique

---
+++ server/controllers/authController.ts
@@ -0,0 +1,69 @@
+import express from 'express'
+import z from 'zod'
+import { User } from '../db/adminDb'
+import jwt, { Secret } from 'jsonwebtoken'
+require('dotenv').config({ path: '../.env' })
+
+let inputProps = z.object({
+  username: z.string().min(1),
+  password: z.string().min(5),
+})
+
+const SECRET_KEY: Secret = process.env.JWT_SECRET as string
+type UserSchema = z.infer<typeof inputProps>
+
+const signup = async (req: express.Request, res: express.Response) => {
+  try {
+    const parsedInput = inputProps.safeParse(req.body)
+    if (!parsedInput.success) {
+      return res.status(400).json({ msg: parsedInput.error })
+    }
+    const newUser = await User.create(req.body)
+
+    const jwtToken = jwt.sign({ newUser }, SECRET_KEY, {
+      expiresIn: '1h',
+    })
+    res.status(200).json({ message: 'Singup Successful', jwtToken })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'An error occured while Signing Up' })
+  }
+}
+
+const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const parsedInput = inputProps.safeParse(req.body)
+    if (!parsedInput.success) {
+      return res.status(400).json({ msg: parsedInput.error })
+    }
+
+    const admin: UserSchema = req.body
+    const username = admin.username
+    const password = admin.password
+    const existingUser = await User.findOne({ username }).select('+password')
+    if (
+      !existingUser ||
+      !existingUser.correctPassword(password, existingUser.password)
+    ) {
+      return res
+        .status(401)
+        .json({ error: 'Either Username or password is not correct ' })
+    }
+    const user = new User({
+      username,
+      password,
+    })
+    const jwtToken = jwt.sign({ user }, SECRET_KEY, {
+      expiresIn: '1h',
+    })
+    res.status(200).json({ message: 'Login Successfully', jwtToken })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'An error occured while Login up' })
+  }
+}
+
+export default {
+  signup,
+  login,
+}
--- server/routes/adminAuth.ts
+++ server/routes/adminAuth.ts
@@ -1,74 +1,7 @@
-import express from 'express'
-const router = express.Router()
-var jwt = require('jsonwebtoken')
-require('dotenv').config()
-import { User } from '../db/adminDb'
-import { z } from 'zod'
+const router = require('express').Router()
+import authController from '../controllers/authController'
 
-let inputProps = z.object({
-  username: z.string().min(1),
-  password: z.string().min(5),
-})
-
-type UserSchema = z.infer<typeof inputProps>
-
-router.post('/signup', async (req, res) => {
-  try {
-    const parsedInput = inputProps.safeParse(req.body)
-    if (!parsedInput.success) {
-      return res.status(400).json({ msg: parsedInput.error })
-    }
-    const admin: UserSchema = req.body
-    const username = admin.username
-    const password = admin.password
-    const userAlreadyExist = await User.findOne({ username })
-    if (userAlreadyExist) {
-      return res.status(500).json({ message: 'User already present' })
-    }
-    const user = await new User({
-      username,
-      password,
-    })
-
-    const jwtToken = jwt.sign({ user }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
-    })
-    await user.save()
-    res.status(200).json({ message: 'Singup Successful', jwtToken })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'An error occured while Signing Up' })
-  }
-})
-
-router.post('/login', async (req, res) => {
-  try {
-    const parsedInput = inputProps.safeParse(req.body)
-    if (!parsedInput.success) {
-      return res.status(400).json({ msg: parsedInput.error })
-    }
-
-    const admin: UserSchema = req.body
-    const username = admin.username
-    const password = admin.password
-    const existingUser = await User.findOne({ username, password })
-    if (!existingUser) {
-      return res
-        .status(404)
-        .json({ error: 'Either Username or password is not correct ' })
-    }
-    const user = await new User({
-      username,
-      password,
-    })
-    const jwtToken = jwt.sign({ user }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
-    })
-    res.status(200).json({ message: 'Login Successfully', jwtToken })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'An error occured while Login up' })
-  }
-})
+router.post('/signup', authController.signup)
+router.post('/login', authController.login)
 
 export default router
