36125d66893a417b63d57451539ffc40e455cc1c
Pratyush Gupta <56015449+pratyushgguptaa@users.noreply.github.com>
2023-12-05 01:47:04 +0530

    setup and teardown

--- server/__test__/adminAuth.test.ts
+++ server/__test__/adminAuth.test.ts
@@ -4,19 +4,25 @@ import * as dotenv from 'dotenv'
 dotenv.config({ path: '../.env.test.local' })
 import mongoose from 'mongoose'
 
-if (process.env.MONGODB_CONNECTION) {
-  mongoose
-    .connect(process.env.MONGODB_CONNECTION)
-    .then(async () => {
-      console.log('Connected to MongoDB')
-      await mongoose.connection.db.dropCollection('users')
-    })
-    .catch((error) => {
-      console.log('Error connecting to MongoDB: ' + error)
-    })
-} else {
-  console.log('Error Connecting to MongoDb, connection string is not present')
-}
+beforeAll(() => {
+  if (process.env.MONGODB_CONNECTION) {
+    mongoose
+      .connect(process.env.MONGODB_CONNECTION)
+      .then(async () => {
+        console.log('Connected to MongoDB')
+        await mongoose.connection.db.dropCollection('users')
+      })
+      .catch((error) => {
+        console.log('Error connecting to MongoDB: ' + error)
+      })
+  } else {
+    console.log('Error Connecting to MongoDb, connection string is not present')
+  }
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
 
 describe('Auth Routes', () => {
   it('should sign up a new user', async () => {
